5c9320ffa2230c8db8e9bd36756b51c618eeb67e
Kristof Szollosi <kristof.szollosi92+git@gmail.com>
2021-01-21 21:37:09 +0100

    add handleClick to Button component

--- src/components/button/ps-button.ts
+++ src/components/button/ps-button.ts
@@ -1,5 +1,13 @@
 class Button extends HTMLElement {
   primary = `"cursor-pointer container w-36 m-4 p-2 bg-turquoise-4 hover:bg-gray-3 hover:shadow-inner rounded-md text-center"`
+  handleClick = () => {}
+
+  constructor() {
+    super()
+
+    this.handleClick = this.handleClick.bind(this)
+    this.addEventListener("click", this.handleClick)
+  }
 
   connectedCallback() {
     this.innerHTML = `<div class=${this.primary}><ps-header size="medium">${this.innerHTML}</ps-header></div>`
