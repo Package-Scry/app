7945abc8ff1c88810ea241c238866fe504f8192e
Kristof Szollosi <kristof.szollosi92+git@gmail.com>
2021-01-22 20:51:41 +0100

    add button column to table

--- src/components/table/ps-table.ts
+++ src/components/table/ps-table.ts
@@ -24,18 +24,19 @@ const COLUMN_NAMES = [
 ]
 
 class Table extends HTMLElement {
-  sizes = {
-    big: "3xl",
-    medium: "xl",
-    small: "sm",
-  }
+  getButton = (
+    type: "primary" | "secondary" | "loading",
+    text: "Update" | "Loading" | "Up to date"
+  ) => `<ps-button type="${type}" class="ml-4">${text}</ps-button>`
 
   connectedCallback() {
     // const data: Data[] = (this.getAttribute("data") ?? []) as Data[]
     const data: Data[] = fakeData
-    const header = `<div class="grid grid-cols-5 grid-flow-col mb-2 bg-turquoise-2">${COLUMN_NAMES.map(
+    const header = `<div class="grid grid-cols-table grid-flow-col mb-4 bg-turquoise-2">${COLUMN_NAMES.map(
       (_, i) =>
-        `<div class="p-2"><ps-header size="medium">${COLUMN_NAMES[i]}</ps-header></ps-header></div>`
+        `<div class="p-2${
+          i === COLUMN_NAMES.length - 1 ? " ml-4" : ""
+        }"><ps-header size="medium">${COLUMN_NAMES[i]}</ps-header></div>`
     ).join("")}</div>`
     const rows = data
       .map((item, i) => {
@@ -46,13 +47,13 @@ class Table extends HTMLElement {
             }"><ps-base>${item[name]}</ps-base></div>`
         )
 
-        return `<div class="grid grid-cols-5 grid-flow-col mb-2">${columns.join(
+        return `<div class="grid grid-cols-table grid-flow-col mb-4">${columns.join(
           ""
-        )}</div>`
+        )}${this.getButton("primary", "Update")}</div>`
       })
       .join("")
 
-    this.innerHTML = `<div class="text-blue-4 text-center mx-12">${header}${rows}</div>`
+    this.innerHTML = `<div class="text-blue-4 text-center mx-12 my-6">${header}${rows}</div>`
   }
 }
 
