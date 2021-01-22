916c04a1bf04f1a41710766b99f4026d1e66cc98
Kristof Szollosi <kristof.szollosi92+git@gmail.com>
2021-01-22 23:53:55 +0100

    add row component

---
+++ src/components/table/ps-row.ts
@@ -0,0 +1,14 @@
+type RowTypes = "default" | "header"
+
+class Row extends HTMLElement {
+  connectedCallback() {
+    const type: RowTypes = (this.getAttribute("type") ?? "default") as RowTypes
+    const style = `grid grid-cols-table grid-flow-col mb-4 ${
+      type === "header" ? "bg-turquoise-2" : ""
+    }`
+
+    this.innerHTML = `<div class="${style}">${this.innerHTML}</div>`
+  }
+}
+
+window.customElements.define("ps-row", Row)
