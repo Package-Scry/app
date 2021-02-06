c7f3dbf79bb91b297e85baab739374a9a67f5e86
Kristof Szollosi <kristof.szollosi92+git@gmail.com>
2021-02-06 19:49:12 +0100

    get tab's name from local storage

--- src/components/tab/ps-tab.ts
+++ src/components/tab/ps-tab.ts
@@ -1,9 +1,10 @@
 class Tab extends HTMLElement {
   connectedCallback() {
+    const activeTab = localStorage.getItem("activeTab")
     const style =
       "relative cursor-pointer bg-gray-11 w-auto h-8 mx-2 px-10 rounded-t font-serif text-white text-lg self-end shadow-drop"
     const xStyle = "absolute -top-1 right-1.5"
-    this.outerHTML = `<span class="${style}"><span class="align-middle">${this.innerHTML}</span><span class="${xStyle}">x</span></span>`
+    this.outerHTML = `<span class="${style}"><span class="align-middle">${activeTab}</span><span class="${xStyle}">x</span></span>`
   }
 }
 
