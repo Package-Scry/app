2cce71a4d9a45b4ebd3ee676e1475f90f57b32c5
Kristof Szollosi <kristof.szollosi92+git@gmail.com>
2021-02-06 12:24:46 +0100

    increase tab's width

--- src/components/tab/ps-tab.ts
+++ src/components/tab/ps-tab.ts
@@ -1,7 +1,7 @@
 class Tab extends HTMLElement {
   connectedCallback() {
     const style =
-      "relative cursor-pointer bg-gray-11 w-auto h-8 mx-2 px-8 rounded-t font-serif text-white text-lg self-end shadow-drop"
+      "relative cursor-pointer bg-gray-11 w-auto h-8 mx-2 px-10 rounded-t font-serif text-white text-lg self-end shadow-drop"
     const xStyle = "absolute -top-1 right-1.5"
     this.outerHTML = `<span class="${style}"><span class="align-middle">${this.innerHTML}</span><span class="${xStyle}">x</span></span>`
   }
