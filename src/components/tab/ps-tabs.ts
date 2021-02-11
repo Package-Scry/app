211037fd21cfe62a684c89ac725ee47918a4edf2
Kristof Szollosi <kristof.szollosi92+git@gmail.com>
2021-02-12 00:30:24 +0100

    create tabs component for data handling

---
+++ src/components/tab/ps-tabs.ts
@@ -0,0 +1,87 @@
+class Tabs extends HTMLElement {
+  activeTab: string
+  tabs: string[]
+
+  constructor() {
+    super()
+
+    this.activeTab = localStorage.getItem("activeTab")
+    this.tabs = localStorage.getItem("tabs")
+      ? localStorage
+          .getItem("tabs")
+          .split(",")
+          .filter(tab => tab)
+      : []
+
+    window.api.receive("packages", (data: { name: string }) => {
+      this.activeTab = data.name
+      localStorage.setItem("activeTab", this.activeTab)
+
+      if (!this.tabs.includes(this.activeTab)) {
+        this.tabs = [...this.tabs, this.activeTab]
+
+        localStorage.setItem("tabs", `${this.tabs.join(",")},`)
+      }
+
+      this.render()
+    })
+  }
+
+  rerenderTable() {
+    const table = document.querySelector<
+      HTMLElement & { shouldRerender: string }
+    >("ps-table")
+
+    table.shouldRerender = "true"
+  }
+
+  close(tab: string) {
+    const replacedTabs = localStorage.getItem("tabs").replace(`${tab},`, "")
+
+    localStorage.setItem("tabs", replacedTabs)
+
+    this.tabs = replacedTabs ? replacedTabs.split(",") : []
+    this.activeTab = replacedTabs ? this.tabs[0] : ""
+
+    localStorage.setItem("activeTab", this.activeTab)
+
+    const path = this.activeTab
+      ? localStorage.getItem(`dirPath-${this.activeTab}`)
+      : null
+
+    window.api.send("workspaceFolder", { path })
+
+    this.rerenderTable()
+    this.render()
+  }
+
+  render() {
+    this.innerHTML = this.tabs
+      .map(
+        tab => `<ps-tab activeTab="${this.activeTab}" name="${tab}"></ps-tab>`
+      )
+      .join("")
+    const tabs = document.querySelectorAll<HTMLElement>("ps-tab")
+    const closeButtons = document.querySelectorAll<HTMLElement>(".close")
+
+    closeButtons.forEach((button, i) =>
+      button.addEventListener("click", e => {
+        e.stopPropagation()
+        this.close(this.tabs[i])
+      })
+    )
+    tabs.forEach(tab =>
+      tab.addEventListener("click", () => {
+        const path = localStorage.getItem(`dirPath-${this.activeTab}`)
+
+        window.api.send("workspaceFolder", { path })
+      })
+    )
+  }
+
+  connectedCallback() {
+    this.render()
+  }
+}
+
+window.customElements.define("ps-tabs", Tabs)
