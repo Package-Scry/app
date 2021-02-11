7891f483421633d115d38e30fcad236af92e03e6
Kristof Szollosi <kristof.szollosi92+git@gmail.com>
2021-02-12 00:29:39 +0100

    move tab to its own component

--- src/components/tab/ps-tab.ts
+++ src/components/tab/ps-tab.ts
@@ -1,35 +1,11 @@
 class Tab extends HTMLElement {
-  constructor() {
-    super()
-
-    this.activeTab = localStorage.getItem("activeTab")
-
-    window.api.receive("packages", (data: TSFixMe) => {
-      this.activeTab = data.name
-    })
-  }
-
-  activeTab: string
-
-  close() {
-    const table = document.querySelector<
-      HTMLElement & { shouldRerender: string }
-    >("ps-table")
-
-    table.shouldRerender = "true"
-    localStorage.clear()
-    window.api.send("workspaceFolder", { path: null })
-  }
-
   connectedCallback() {
+    const activeTab: string = this.getAttribute("activeTab")
+    const name: string = this.getAttribute("name")
     const style =
       "relative cursor-pointer bg-gray-11 w-auto h-8 mx-2 px-10 rounded-t font-serif text-white text-lg self-end shadow-drop"
-    const xStyle = "absolute -top-1 right-1.5"
-    this.outerHTML = `<span class="${style}"><span class="align-middle">${this.activeTab}</span><span class="${xStyle}">x</span></span>`
-
-    const span = document.querySelectorAll<HTMLElement>("span")[2]
-
-    span.addEventListener("click", this.close)
+    const xStyle = "close absolute -top-1 right-1.5"
+    this.innerHTML = `<span class="${style}"><span class="align-middle">${name}</span><span class="${xStyle}">x</span></span>`
   }
 }
 
