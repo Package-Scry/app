f4caa3e0ac51b3293d4c20210037fad9615e5e46
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-04-06 19:09:36 +0200

    add basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+module.exports = [
+  {
+    mode: "development",
+    entry: "./src/main.ts",
+    target: "electron-main",
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          include: /src/,
+          use: [{ loader: "ts-loader" }],
+        },
+      ],
+    },
+    output: {
+      path: __dirname + "/dist",
+      filename: "electron.js",
+    },
+  },
+]
