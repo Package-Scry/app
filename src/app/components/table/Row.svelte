74425e5ea6bc26b238c611337e9e86bd365f482f
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-06-27 15:47:04 +0200

    add row component

---
+++ src/app/components/table/Row.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let style = "grid grid-cols-table grid-rows-table grid-flow-col mb-4 "
+</script>
+
+<li class="px-2 truncate h-full flex items-center {style}">
+  <slot>loading</slot>
+</li>
