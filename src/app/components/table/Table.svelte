3e01a3173c3484d91643aa35aa2173e069e1246b
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-06-27 15:47:50 +0200

    add table component

---
+++ src/app/components/table/Table.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import Column from "./Column.svelte"
+  import Row from "./Row.svelte"
+  import type { Column as ColumnType, Data } from "./index"
+
+  export let data: Data
+  export let columns: ColumnType[]
+</script>
+
+<div class="text-center mx-12 mt-6 mb-20">
+  <ul>
+    <slot />
+    {#each data as rowData, i (rowData.name)}
+      <Row>
+        {#each columns as column}
+          <Column
+            {rowData}
+            {...column}
+            style={i % 2 === 0 ? "bg-gray-4" : undefined}
+          />
+        {/each}
+      </Row>
+    {/each}
+  </ul>
+</div>
