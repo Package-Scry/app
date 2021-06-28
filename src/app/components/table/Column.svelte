b796c61bfebe7d0c6aae8300c727d11fdb97ebab
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-06-28 20:59:05 +0200

    fix render styling

--- src/app/components/table/Column.svelte
+++ src/app/components/table/Column.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
   import type { Column, Row } from "./index"
 
+  export let render: Column["render"] = undefined
   export let style = "bg-turquoise-2"
   export let dataKey: Column["dataKey"] = undefined
-  export let render: Column["render"] = undefined
   export let onClick: Column["onClick"] = () => {}
-  export let rowData: Row
+  export let rowData: Row = {}
 </script>
 
 <div
-  class="px-2 truncate h-full flex items-center text-white text-xl font-serif {style}"
+  class="px-2 truncate h-full flex items-center text-white text-xl font-serif {!render
+    ? style
+    : ''}"
   on:click={onClick}
 >
   {#if !!dataKey}
