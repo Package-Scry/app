a0c085424749378ca833b7048cf01aee23a7d5eb
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-03 14:30:18 +0200

    add Header typo component

---
+++ src/app/components/typography/Header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  enum SIZES {
+    big = "3xl",
+    medium = "xl",
+    small = "sm",
+  }
+
+  export let size: keyof typeof SIZES = "big"
+  const style = `text-white text-${SIZES[size]} font-serif`
+</script>
+
+<span class={style}><slot /></span>
--- src/app/components/typography/index.ts
+++ src/app/components/typography/index.ts
@@ -1,1 +1,2 @@
 export { default as Base } from "./Base.svelte"
+export { default as Header } from "./Header.svelte"
