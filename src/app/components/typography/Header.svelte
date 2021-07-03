7a6a8979133db7d5a15b7f608e4a2e96c9e2b894
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-03 16:33:15 +0200

    add custom style & optional shadow prop

--- src/app/components/typography/Header.svelte
+++ src/app/components/typography/Header.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
   enum SIZES {
+    xl = "4xl",
     big = "3xl",
     medium = "xl",
     small = "sm",
   }
 
+  export let style: string = undefined
   export let size: keyof typeof SIZES = "big"
-  const style = `text-white text-${SIZES[size]} font-serif`
+  export let hasShadow = false
+  const defaultStyle = `block text-white text-${SIZES[size]} font-serif${
+    hasShadow ? " text-shadow" : ""
+  }`
+  const finalStyle = `${defaultStyle}${style ?? ""}`
 </script>
 
-<span class={style}><slot /></span>
+<span class={finalStyle}><slot /></span>
