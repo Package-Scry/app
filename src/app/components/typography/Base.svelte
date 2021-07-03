572cd02beb6d6df5f6d4ac814b60a7f5fbdc7666
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-03 17:02:47 +0200

    fix missing whitespace

--- src/app/components/typography/Base.svelte
+++ src/app/components/typography/Base.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   export let style: string = undefined
   const defaultStyle = `text-white text-xl font-serif`
-  const finalStyle = `${defaultStyle}${style ?? ""}`
+  const finalStyle = `${defaultStyle} ${style ?? ""}`
 </script>
 
 <span class={finalStyle}><slot /></span>
