19d170ff641067f2554940c889160791fe049c6d
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-03 13:42:55 +0200

    add disabled state to button

--- src/app/components/button/Button.svelte
+++ src/app/components/button/Button.svelte
@@ -4,17 +4,20 @@
   export let text: string
   export let type: Type = "primary"
   export let onClick: () => void
+  export let isDisabled: boolean
 
   const styles = {
-    common: `cursor-pointer select-none rounded-md text-center w-full h-full flex items-center justify-center`,
+    common: `select-none rounded-md text-center w-full h-full flex items-center justify-center`,
     primary: `bg-turquoise-4 hover:bg-gray-3 hover:shadow-inner`,
     secondary: `border-2 border-turquoise-5`,
     loading: `bg-gray-6`,
   }
 
-  $: style = `${styles.common} ${styles[type]}`
+  $: style = `${styles.common} ${styles[type]}${
+    isDisabled ? "" : " cursor-pointer"
+  }`
 </script>
 
-<div class={style} on:click={onClick}>
+<div class={style} on:click={isDisabled ? () => {} : onClick}>
   <span>{text}</span>
 </div>
