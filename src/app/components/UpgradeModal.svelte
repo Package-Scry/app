ccf51ea91746b41ec4fad4a78d9c83e70ce036c3
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-03 16:42:42 +0200

    add upgrade modal

---
+++ src/app/components/UpgradeModal.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Modal from "./components/modal/Modal.svelte"
+  import Base from "./components/typography/Base.svelte"
+
+  // TODO: get this from store
+  export let isVisible = false
+</script>
+
+<Modal
+  headerText="You've discovered a Pro featutre!"
+  buttonText="Upgrade to Pro"
+  cancelText="Skip upgrade for now"
+  onCancel={() => {
+    isVisible = false
+  }}
+  onClick={() => {
+    window.api.send("upgrade", {})
+    isVisible = false
+  }}
+  {isVisible}
+>
+  <div slot="content">
+    <Base style="mt-4 text-xl">
+      With Pro you can enjoy Unlimited Workspaces.
+    </Base>
+  </div>
+</Modal>
