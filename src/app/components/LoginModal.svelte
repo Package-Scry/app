9936a25f6b88b93154d6d7f9a5d5ed3dc73b8f83
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-03 17:28:32 +0200

    add Login Modal

---
+++ src/app/components/LoginModal.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Modal from "./components/modal/Modal.svelte"
+
+  // TODO: get this from store
+  export let isVisible = false
+</script>
+
+<Modal
+  headerText="Login"
+  buttonText="Login with GitHub"
+  buttonIcon="github"
+  onClick={() => {
+    window.api.send("authenticate", {})
+
+    isVisible = false
+  }}
+  {isVisible}
+/>
