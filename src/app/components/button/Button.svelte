0ae946cf3b43d1b2b15dd19f2ac2e5db66f1737a
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-03 16:32:22 +0200

    add modal type button

--- src/app/components/button/Button.svelte
+++ src/app/components/button/Button.svelte
@@ -1,20 +1,21 @@
 <script lang="ts">
   import { Header } from "../typography"
 
-  type Type = "primary" | "secondary" | "loading"
+  type Type = "primary" | "secondary" | "loading" | "modal"
 
   export let type: Type = "primary"
   export let onClick: () => void
-  export let isDisabled: boolean
+  export let isDisabled = false
 
   const styles = {
     common: `select-none rounded-md text-center w-full h-full flex items-center justify-center`,
     primary: `bg-turquoise-4 hover:bg-gray-3 hover:shadow-inner`,
     secondary: `border-2 border-turquoise-5`,
     loading: `bg-gray-6`,
+    modal: `cursor-pointer inline-block bg-black-3 py-4 px-10 mt-18 rounded-lg hover:bg-black-5`,
   }
 
-  $: style = `${styles.common} ${styles[type]}${
+  $: style = `${type === "modal" ? "" : styles.common} ${styles[type]}${
     isDisabled ? "" : " cursor-pointer"
   }`
 </script>
