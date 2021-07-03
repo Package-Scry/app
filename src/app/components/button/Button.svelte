c9edd04d6d32df424951a8d753b5392e09f7625b
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-03 17:25:53 +0200

    add icon prop to button

--- src/app/components/button/Button.svelte
+++ src/app/components/button/Button.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
+  import GitHub from "../icon/GitHub.svelte"
+
   import { Header } from "../typography"
 
   type Type = "primary" | "secondary" | "loading" | "modal"
 
   export let type: Type = "primary"
+  export let icon: string = undefined
   export let onClick: () => void
   export let isDisabled = false
+
+  const icons = {
+    github: GitHub,
+  }
 
   const styles = {
     common: `select-none rounded-md text-center w-full h-full flex items-center justify-center`,
     primary: `bg-turquoise-4 hover:bg-gray-3 hover:shadow-inner`,
     secondary: `border-2 border-turquoise-5`,
     loading: `bg-gray-6`,
-    modal: `cursor-pointer inline-block bg-black-3 py-4 px-10 mt-14 rounded-lg hover:bg-black-5`,
+    modal: `cursor-pointer inline-block bg-black-3 py-${
+      icon ? 2 : 4
+    } px-10 mt-14 rounded-lg hover:bg-black-5`,
   }
 
   $: style = `${type === "modal" ? "" : styles.common} ${styles[type]}${
@@ -21,5 +30,6 @@
 </script>
 
 <div class={style} on:click={isDisabled ? () => {} : onClick}>
-  <Header size="medium"><slot /></Header>
+  <svelte:component this={icons[icon]} class="inline-block" />
+  <Header size="medium" style="inline-block"><slot /></Header>
 </div>
