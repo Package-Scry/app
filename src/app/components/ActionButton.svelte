2bb05fb1ca34079a8c1f5998c9d4dbd36aea7bc6
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-03 13:45:03 +0200

    add functionality to ActionButton

--- src/app/components/ActionButton.svelte
+++ src/app/components/ActionButton.svelte
@@ -1,17 +1,30 @@
 <script lang="ts">
   import Button from "./button/Button.svelte"
-  import type Row from "./Table/index"
+  import { updatePackage } from "./store"
 
   type ButtonText = "Update" | "Loading" | "Up to date"
   type ButtonTypes = "primary" | "secondary" | "loading"
   type Status = "loading" | "up to date" | "updatable"
 
-  export let rowData: { status: Status; name: string }
-  export let onClick = () => {
-    console.log(rowData.name)
+  export let rowData: {
+    status: Status
+    name: string
+    version: string
+    wanted: string
+    latest: string
+    local: string
   }
 
   $: status = rowData.status
+  $: packageName = rowData.name
+  $: localVersion = rowData.local
+  $: wantedVersion = rowData.wanted
+  $: latestVersion = rowData.latest
+  $: targetVersion =
+    wantedVersion !== latestVersion &&
+    wantedVersion !== localVersion.replace("^", "")
+      ? wantedVersion
+      : latestVersion
   $: type =
     status === "loading"
       ? "loading"
@@ -24,6 +37,9 @@
       : status === "up to date"
       ? "Up to date"
       : ("Update" as ButtonText)
+  $: isDisabled = ["loading", "secondary"].includes(type)
+
+  const onClick = () => updatePackage(packageName, targetVersion)
 </script>
 
-<Button {text} {type} {status} {onClick} />
+<Button {text} {type} {onClick} {isDisabled} />
