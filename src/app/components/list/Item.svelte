0c969d170307c95f981635862bbc1a4d2fb4493b
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-03 17:02:26 +0200

    add list item component

---
+++ src/app/components/list/Item.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Tick from "./Tick.svelte"
+</script>
+
+<li class="grid grid-flow-col items-center grid-cols-feature gap-x-5">
+  <div><Tick /></div>
+  <div><slot /></div>
+</li>
---
+++ src/app/components/list/Tick.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<svg
+  width="1rem"
+  height="1rem"
+  viewBox="0 0 36 33"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path d="M2 18.0787L10.9492 29L34 2" stroke="white" stroke-width="4" />
+</svg>
