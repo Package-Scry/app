afb6709a651f5fe38d3df02b81f72682c91e0ab7
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-04 20:58:21 +0200

    add workspace store

---
+++ src/app/components/stores/workspace.ts
@@ -0,0 +1,55 @@
+import { writable } from "svelte/store"
+
+const getWorkspaces = () =>
+  localStorage.getItem("tabs")
+    ? localStorage
+        .getItem("tabs")
+        .split(",")
+        .filter(tab => tab)
+    : []
+
+export const workspaces = writable<string[]>(getWorkspaces())
+export const activeTab = writable<string>(localStorage.getItem("activeTab"))
+
+export const changeActive = (
+  newActiveTab: string,
+  currentWorkspaces: string[]
+): void => {
+  activeTab.set(newActiveTab)
+
+  localStorage.setItem("activeTab", newActiveTab)
+
+  if (!currentWorkspaces.includes(newActiveTab)) {
+    workspaces.update(value => {
+      const newWorkspaces = [...value, newActiveTab]
+
+      localStorage.setItem("tabs", `${currentWorkspaces.join(",")},`)
+
+      return newWorkspaces
+    })
+  }
+}
+
+export const close = (tab: string, isTabActive: boolean): void => {
+  const replacedTabs = localStorage.getItem("tabs").replace(`${tab},`, "")
+  const newTabs = replacedTabs ? replacedTabs.split(",").filter(tab => tab) : []
+
+  localStorage.setItem("tabs", replacedTabs)
+  localStorage.removeItem(`dirPath-${tab}`)
+
+  workspaces.set(newTabs)
+
+  if (isTabActive) {
+    const newActiveTab = replacedTabs ? newTabs[0] : ""
+
+    localStorage.setItem("activeTab", newActiveTab)
+
+    activeTab.set(newActiveTab)
+
+    const path = newActiveTab
+      ? localStorage.getItem(`dirPath-${newActiveTab}`)
+      : null
+
+    window.api.send("workspaceFolder", { path })
+  }
+}
