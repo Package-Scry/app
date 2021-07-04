c8e1a77fb481284b97a8ee8d11aedcf3fd5e9445
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-04 20:53:39 +0200

    add Tab component

---
+++ src/app/components/tab/Tab.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import Base from "../typography/Base.svelte"
+
+  export let activeTab: string
+  export let name: string
+  export let onClick: (tabName: string, isTabActive: boolean) => void
+  export let onClose: (tabName: string, isTabActive: boolean) => void
+
+  const tabStyle =
+    "align-middle relative cursor-pointer px-10 rounded-t font-serif text-white text-lg self-end shadow-drop mr-0.5 first:ml-2"
+  const activeStyle = "bg-black-2 h-9 leading-loose hover:bg-black-6"
+
+  $: isActive = name === activeTab
+  $: style = isActive
+    ? `${activeStyle} ${tabStyle}`
+    : `${tabStyle} bg-gray-11 h-8 hover:bg-gray-12`
+</script>
+
+<span class={style} on:click={() => onClick(name, isActive)}>{name}</span>
+<span
+  class="close absolute -top-1 right-1 font-sans-serif text-base font-thin"
+  on:click={() => onClose(name, isActive)}
+>
+  x
+</span>
