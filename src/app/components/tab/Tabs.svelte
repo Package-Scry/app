efff269529cb8c20e3c806b2f57ac13027c993fb
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-04 20:56:37 +0200

    add Tabs component

---
+++ src/app/components/tab/Tabs.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Tab from "./Tab.svelte"
+
+  export let tabs: string[] = []
+  export let activeTab: string
+  export let onTabClick: (tabName: string, isTabActive: boolean) => void
+  export let onTabClose: (tabName: string, isTabActive: boolean) => void
+</script>
+
+<div class="flex flex-row bg-gray-4 w-full h-10 top-0">
+  {#each tabs as tab}
+    <Tab name={tab} {activeTab} onClose={onTabClose} onClick={onTabClick} />
+  {/each}
+</div>
