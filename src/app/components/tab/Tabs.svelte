9a5d154846c8fb271e159beacf08d16aaa098cf7
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-05 18:33:23 +0200

    add scroll to tabs

--- src/app/components/tab/Tabs.svelte
+++ src/app/components/tab/Tabs.svelte
@@ -5,9 +5,27 @@
   export let activeTab: string
   export let onTabClick: (tabName: string, isTabActive: boolean) => void
   export let onTabClose: (tabName: string, isTabActive: boolean) => void
+
+  let elTabs: HTMLElement
+
+  const onWheel = (e: WheelEvent) => {
+    e.preventDefault()
+
+    const tabsScrollPosition = elTabs.scrollLeft
+
+    elTabs.scrollTo({
+      top: 0,
+      left: tabsScrollPosition + e.deltaY,
+      behavior: "smooth",
+    })
+  }
 </script>
 
-<div class="flex flex-row bg-gray-4 w-full h-10 top-0">
+<div
+  class="flex flex-row bg-gray-4 w-full h-10 top-0 overflow-hidden"
+  on:wheel={onWheel}
+  bind:this={elTabs}
+>
   {#each tabs as tab}
     <Tab name={tab} {activeTab} onClose={onTabClose} onClick={onTabClick} />
   {/each}
