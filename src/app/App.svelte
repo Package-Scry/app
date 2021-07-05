0a3e24709ecfb7a9177b7117284c5b05684f209e
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-05 19:38:50 +0200

    add components to App

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
   import PackageTable from "./components/PackageTable.svelte"
+  import Workspaces from "./components/Workspaces.svelte"
+  import { isLoggedIn } from "./components/stores/user"
+  import LoginModal from "./components/LoginModal.svelte"
+  import ProFeatureModal from "./components/ProFeatureModal.svelte"
+  import UpgradeModal from "./components/UpgradeModal.svelte"
+  import AuthHandler from "./components/AuthHandler.svelte"
 </script>
 
-<PackageTable />
+<AuthHandler />
+
+{#if $isLoggedIn}
+  <Workspaces />
+  <PackageTable />
+  <ProFeatureModal />
+  <UpgradeModal />
+{/if}
+
+<LoginModal />
