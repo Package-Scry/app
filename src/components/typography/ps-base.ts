34e33fb210e13e37d28500127bab9a61854754ee
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-13 19:44:53 +0200

    remove unused web components

--- src/components/typography/ps-base.ts
+++
@@ -1,7 +0,0 @@
-class Base extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `<span class="text-white text-xl font-serif">${this.innerHTML}</span>`
-  }
-}
-
-window.customElements.define("ps-base", Base)
