1b3e2d1da4e88e53d87cdefa3f494c49e162fb76
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-13 19:58:55 +0200

    add bottom stripe to app

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -6,6 +6,7 @@
   import ProFeatureModal from "./components/ProFeatureModal.svelte"
   import UpgradeModal from "./components/UpgradeModal.svelte"
   import AuthHandler from "./components/AuthHandler.svelte"
+  import UpgradeButton from "./components/UpgradeButton.svelte"
 </script>
 
 <AuthHandler />
@@ -16,5 +17,20 @@
   <ProFeatureModal />
   <UpgradeModal />
 {/if}
+
+<div
+  class="flex flex-row-reverse bg-gray-4 fixed w-full h-13 bottom-0 shadow-inner-2"
+>
+  <span
+    class="bg-black-1 w-auto mx-5 py-1 px-8 rounded font-serif text-white text-xl self-center"
+    >1.0.1</span
+  >
+  <span
+    class="cursor-pointer bg-black-1 w-auto mx-2 py-1 px-6 rounded font-serif text-white text-lg self-center hover:bg-black-5"
+  >
+    Feedback
+  </span>
+  <UpgradeButton />
+</div>
 
 <LoginModal />
