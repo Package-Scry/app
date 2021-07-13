32d25da4993a010716aac3b6cb1058404238cde4
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-13 19:56:55 +0200

    add UpgradeButton

---
+++ src/app/components/UpgradeButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Scry from "./icon/Scry.svelte"
+
+  const style =
+    "cursor-pointer bg-green-1 w-auto mr-auto ml-5 py-1 px-4 rounded font-serif text-white text-xl self-center hover:bg-green-2"
+  const onClick = () => {
+    window.api.send("upgrade", {})
+  }
+</script>
+
+<span class={style} on:click={onClick}>
+  <span class="text-white fill-current"><Scry /></span>
+  <span class="align-middle ml-2.5">Upgrade to Pro</span>
+</span>
