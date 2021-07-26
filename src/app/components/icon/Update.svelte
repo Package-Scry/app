4425bb72352e4bc2d869a2adbd9c2eb3a9a9dd8d
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-07-26 19:16:43 +0200

    add update icon

---
+++ src/app/components/icon/Update.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+  // TODO: remove temp svg, add final one
+
+  export let style = ""
+</script>
+
+<svg
+  width="32"
+  height="32"
+  viewBox="0 0 40 32"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class={style}
+>
+  <g filter="white">
+    <g filter="white">
+      <path
+        d="M20.6718 8.32825C20.3008 7.95725 19.6992 7.95725 19.3282 8.32825L13.2825 14.374C12.9115 14.745 12.9115 15.3465 13.2825 15.7175C13.6535 16.0885 14.255 16.0885 14.626 15.7175L20 10.3435L25.374 15.7175C25.745 16.0885 26.3465 16.0885 26.7175 15.7175C27.0885 15.3465 27.0885 14.745 26.7175 14.374L20.6718 8.32825ZM20.95 23L20.95 9H19.05L19.05 23H20.95Z"
+        fill="white"
+      />
+    </g>
+    <rect
+      x="5"
+      y="1"
+      width="30"
+      height="30"
+      rx="7"
+      stroke="white"
+      stroke-width="2"
+    />
+  </g>
+  <defs>
+    <filter
+      id="filter0_d"
+      x="0"
+      y="0"
+      width="40"
+      height="40"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset dy="4" />
+      <feGaussianBlur stdDeviation="2" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+      />
+      <feBlend
+        mode="normal"
+        in2="BackgroundImageFix"
+        result="effect1_dropShadow"
+      />
+      <feBlend
+        mode="normal"
+        in="SourceGraphic"
+        in2="effect1_dropShadow"
+        result="shape"
+      />
+    </filter>
+    <filter
+      id="filter1_d"
+      x="9.00391"
+      y="8.0498"
+      width="21.9915"
+      height="22.95"
+      filterUnits="userSpaceOnUse"
+      color-interpolation-filters="sRGB"
+    >
+      <feFlood flood-opacity="0" result="BackgroundImageFix" />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset dy="4" />
+      <feGaussianBlur stdDeviation="2" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+      />
+      <feBlend
+        mode="normal"
+        in2="BackgroundImageFix"
+        result="effect1_dropShadow"
+      />
+      <feBlend
+        mode="normal"
+        in="SourceGraphic"
+        in2="effect1_dropShadow"
+        result="shape"
+      />
+    </filter>
+  </defs>
+</svg>
