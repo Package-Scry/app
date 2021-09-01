c11cca7a0bcb37af3f38fdb82561caa1a7ed119a
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-09-01 11:51:36 +0200

    update update icon svg

--- src/app/components/icon/Update.svelte
+++ src/app/components/icon/Update.svelte
@@ -1,102 +1,29 @@
 <script lang="ts">
-  // TODO: remove temp svg, add final one
-
   export let style = ""
 </script>
 
 <svg
-  width="32"
-  height="32"
-  viewBox="0 0 40 40"
-  fill="none"
+  width="30"
+  height="30"
+  viewBox="0 0 32 32"
   xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  xml:space="preserve"
+  style="fill-rule:evenodd;clip-rule:evenodd;"
   class={style}
 >
-  <g filter="url(#filter_update0_d)">
-    <g filter="url(#filter_update1_d)">
-      <path
-        d="M20.6718 8.32825C20.3008 7.95725 19.6992 7.95725 19.3282 8.32825L13.2825 14.374C12.9115 14.745 12.9115 15.3465 13.2825 15.7175C13.6535 16.0885 14.255 16.0885 14.626 15.7175L20 10.3435L25.374 15.7175C25.745 16.0885 26.3465 16.0885 26.7175 15.7175C27.0885 15.3465 27.0885 14.745 26.7175 14.374L20.6718 8.32825ZM20.95 23L20.95 9H19.05L19.05 23H20.95Z"
-        fill="white"
-      />
-    </g>
-    <rect
-      x="5"
-      y="1"
-      width="30"
-      height="30"
-      rx="7"
-      stroke="white"
-      stroke-width="2"
+  <g transform="matrix(0.966667,0,0,0.966667,-22.6667,0.9925)">
+    <path
+      d="M40.672,8.328C40.301,7.957 39.699,7.957 39.328,8.328L33.283,14.374C32.911,14.745 32.911,15.347 33.283,15.717C33.654,16.089 34.255,16.089 34.626,15.717L40,10.344L45.374,15.717C45.745,16.089 46.347,16.089 46.718,15.717C47.089,15.347 47.089,14.745 46.718,14.374L40.672,8.328ZM40.95,23L40.95,9L39.05,9L39.05,23L40.95,23Z"
+      style="fill:white;fill-rule:nonzero;"
     />
   </g>
-  <defs>
-    <filter
-      id="filter_update0_d"
-      x="0"
-      y="0"
-      width="40"
-      height="40"
-      filterUnits="userSpaceOnUse"
-      color-interpolation-filters="sRGB"
-    >
-      <feFlood flood-opacity="0" result="BackgroundImageFix" />
-      <feColorMatrix
-        in="SourceAlpha"
-        type="matrix"
-        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-        result="hardAlpha"
-      />
-      <feOffset dy="4" />
-      <feGaussianBlur stdDeviation="2" />
-      <feColorMatrix
-        type="matrix"
-        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-      />
-      <feBlend
-        mode="normal"
-        in2="BackgroundImageFix"
-        result="effect1_dropShadow"
-      />
-      <feBlend
-        mode="normal"
-        in="SourceGraphic"
-        in2="effect1_dropShadow"
-        result="shape"
-      />
-    </filter>
-    <filter
-      id="filter_update1_d"
-      x="9.00391"
-      y="8.0498"
-      width="21.9915"
-      height="22.95"
-      filterUnits="userSpaceOnUse"
-      color-interpolation-filters="sRGB"
-    >
-      <feFlood flood-opacity="0" result="BackgroundImageFix" />
-      <feColorMatrix
-        in="SourceAlpha"
-        type="matrix"
-        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-        result="hardAlpha"
-      />
-      <feOffset dy="4" />
-      <feGaussianBlur stdDeviation="2" />
-      <feColorMatrix
-        type="matrix"
-        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-      />
-      <feBlend
-        mode="normal"
-        in2="BackgroundImageFix"
-        result="effect1_dropShadow"
-      />
-      <feBlend
-        mode="normal"
-        in="SourceGraphic"
-        in2="effect1_dropShadow"
-        result="shape"
-      />
-    </filter>
-  </defs>
+  <g transform="matrix(0.966667,0,0,0.966667,-22.6667,0.533333)">
+    <circle
+      cx="40"
+      cy="16"
+      r="15"
+      style="fill:none;stroke:white;stroke-width:2px;"
+    />
+  </g>
 </svg>
