682e54b4f0b6b303d64e3448ce50919e96726d6f
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-08-18 17:48:30 +0200

    use enums for send & receive

--- src/preload.ts
+++ src/preload.ts
@@ -1,46 +1,17 @@
 import { contextBridge, ipcRenderer } from "electron"
-import type { ReceiveChannel, SendChannel } from "../custom"
+import { ReceiveChannels, SendChannels } from "../custom"
 
 // https://github.com/reZach/secure-electron-template/blob/master/docs/newtoelectron.md
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld("api", {
-  send: (channel: SendChannel, data: TSFixMe) => {
-    // whitelist channel
-    const validChannels: SendChannel[] = [
-      "workspaceFolder",
-      "packageUpdate",
-      "outdated",
-      "cancelled",
-      "authenticate",
-      "isLoggedIn",
-      "token",
-      "logout",
-      "upgrade",
-      "alert",
-      "proFeature",
-      "updateAll",
-      "feedback",
-    ]
-
-    if (validChannels.includes(channel)) {
+  send: (channel: SendChannels, data: TSFixMe) => {
+    if (Object.values(SendChannels).includes(channel)) {
       ipcRenderer.send(channel, data)
     }
   },
-  receive: (channel: ReceiveChannel, func: TSFixMe) => {
-    const validChannels: ReceiveChannel[] = [
-      "packages",
-      "packageUpdated",
-      "outdated",
-      "cancelled",
-      "saveToken",
-      "logout",
-      "alert",
-      "proFeature",
-      "updatedAll",
-    ]
-
-    if (validChannels.includes(channel)) {
+  receive: (channel: ReceiveChannels, func: TSFixMe) => {
+    if (Object.values(ReceiveChannels).includes(channel)) {
       // Deliberately strip event as it includes `sender`
       // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return
       ipcRenderer.on(channel, (event, ...args) => func(...args))
