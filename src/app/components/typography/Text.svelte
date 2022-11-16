2920c2827f66a28c80ad85e7ad542858ad73918f
kristofszollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-11-16 20:37:01 +0100

    feat: add text component

---
+++ src/app/components/typography/Text.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let style: string = undefined
+  const defaultStyle = `text-white text-l font-serif`
+  const finalStyle = `${defaultStyle} ${style ?? ""}`
+</script>
+
+<span class={finalStyle}><slot /></span>
